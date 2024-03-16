db51249c4580cebfda4d1da2a937baf8315060f2
Prince Mensah <princemensaheyram@gmail.com>
2024-03-16 12:18:31 +0000

    uploaded test folder

--- tests/linear_reg_test.py
+++ tests/linear_reg_test.py
@@ -10,23 +10,33 @@ X , y = datasets.make_regression(n_samples=100, n_features=1, noise=10, random_s
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) # split data for training.
 
 # Model
-regressor = LinearRegression(learning_rate=0.01, n_iterations=1000)
+model = LinearRegression(learning_rate=0.01, n_iterations=1000)
 
-regressor.fit(X_train, y_train) # fitting the model
-predictions = regressor.predict(X_test)# making predictions
+model.fit(X_train, y_train) # fitting the model
+predictions = model.predict(X_test)# making predictions
 
 # model evaluation
-mse = regressor.mean_squared_error(y_test, predictions)
-accuracy = regressor.r2_score(y_test, predictions)
-print(mse)
-print(accuracy)
+mse = model.mean_squared_error(y_test, predictions)
+accuracy = model.r2_score(y_test, predictions)
+print('MSE: ', mse)
+print('Accuracy: ', accuracy)
+
+# Training error plot
+n_points = len(model.losses)
+training, = plt.plot(range(n_points), model.losses, label="Training Error", color='blue')
+plt.legend(handles=[training])
+plt.title("Error Plot")
+plt.ylabel('Error')
+plt.xlabel('Iterations')
+plt.show()
 
 #plot results
-predictions_line = regressor.predict(X)
+predictions_line = model.predict(X)
+cmap = plt.get_cmap('Blues') #others: viridis, magma, plasma, cividis, inferno
 fig = plt.figure(figsize=(8, 5))
-r1 = plt.scatter(X_train, y_train, label='train set')
-r2 = plt.scatter(X_test, y_test, label='test set')
-plt.plot(X, predictions_line, color='black', label="best fit line")
-plt.legend()
+r1 = plt.scatter(X_train, y_train, label='train sample', color=cmap(0.5), s=12)
+r2 = plt.scatter(X_test, y_test, label='test sample', color=cmap(0.9), s=12)
+plt.plot(X, predictions_line, color='black', label="line of best fit", linewidth=2)
+plt.legend(loc='lower right')
 plt.show()
 
